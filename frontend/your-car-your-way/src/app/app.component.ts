7604e21982cda3d3bac15d7e8f0b717cc71d505f
Geoffrey Ouvrard <geoffrey.1796@gmail.com>
2023-10-04 15:13:14 +0200

    added logout button on home page on frontend side

--- frontend/your-car-your-way/src/app/app.component.ts
+++ frontend/your-car-your-way/src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoginService } from './services/login.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'your-car-your-way';
+export class AppComponent implements OnInit {
+  constructor(private loginService: LoginService) { }
+
+  ngOnInit(): void {
+    this.loginService.tryAutoLogin();
+  }
+
 }
